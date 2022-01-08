1a15aa0ce836c26deb685d091d6be608834caf04
Coordinate-Cat <tetrapasta02@gmail.com>
2022-01-08 16:50:26 +0900

    ✨ :update, osint list

--- osint-resource.sh
+++ osint-resource.sh
@@ -1,3 +1,5 @@
+#!/bin/sh
+
 . sentaku -n
 
 _SENTAKU_SEPARATOR=$'\n'
@@ -8,28 +10,14 @@ _SENTAKU_NONUMBER=1
 menu="
    Github    : (https://github.com)
    test      : (https://twitter.com)
-   Instagram : (https://www.instagram.com)
-   Reddit    : (https://www.reddit.com)
-   Facebook  : (https://www.facebook.com)
-   Pinterest : (https://jp.pinterest.com)
-   Telegram  : (https://web.telegram.org)
-   Keybase   : (https://keybase.io)
-  ﭮ Discord   : (https://discord.com)
-   5ch       : (https://5ch.net)
+  exit<<<CONTROL + C
 "
 
 # SNS URL PUSH
-_sf_1 ()  { open https://github.com        }
-_sf_2 ()  { open https://twitter.com/      }
-_sf_3 ()  { open https://www.instagram.com }
-_sf_4 ()  { open https://www.reddit.com    }
-_sf_5 ()  { open https://www.facebook.com  }
-_sf_6 ()  { open https://jp.pinterest.com  }
-_sf_7 ()  { open https://web.telegram.org  }
-_sf_8 ()  { open https://keybase.io        }
-_sf_9 ()  { open https://discord.com       }
-_sf_10 () { open https://5ch.net           }
+_sf_1 ()  { open https://github.com | exit }
+_sf_2 ()  { open https://twitter.com/ | exit }
 
+# FUNCTIONS
 func (){
   _sf_1 () {
     _s_current_n=0
@@ -39,34 +27,6 @@ func (){
     _s_current_n=1
     _s_break=1
   }
-  _sf_3 () {
-    _s_current_n=2
-    _s_break=1
-  }
-  _sf_4 () {
-    _s_current_n=3
-    _s_break=1
-  }
-  _sf_5 () {
-    _s_current_n=4
-    _s_break=1
-  }
-  _sf_6 () {
-    _s_current_n=5
-    _s_break=1
-  }
-  _sf_7 () {
-    _s_current_n=6
-    _s_break=1
-  }
-  _sf_8 () {
-    _s_current_n=7
-    _s_break=1
-  }
-  _sf_9 () {
-    _s_current_n=8
-    _s_break=1
-  }
 }
 
 _sf_select () {
@@ -74,20 +34,6 @@ _sf_select () {
     _sf_1
   elif [ $_s_current_n -eq 1 ];then
     _sf_2
-  elif [ $_s_current_n -eq 2 ];then
-    _sf_3
-  elif [ $_s_current_n -eq 3 ];then
-    _sf_4
-  elif [ $_s_current_n -eq 4 ];then
-    _sf_5
-  elif [ $_s_current_n -eq 5 ];then
-    _sf_6
-  elif [ $_s_current_n -eq 6 ];then
-    _sf_7
-  elif [ $_s_current_n -eq 7 ];then
-    _sf_8
-  elif [ $_s_current_n -eq 8 ];then
-    _sf_9
   fi
 }
 
--- osint.sh
+++ osint.sh
@@ -1,70 +1,73 @@
+#!/bin/sh
+
 . sentaku -n
 
 _SENTAKU_SEPARATOR=$'\n'
 _SENTAKU_NOHEADER=1
 _SENTAKU_NONUMBER=1
 shopt -s expand_aliases
-alias resource='zsh /Users/ocat/dotfiles/.osint/OSINT-TOOLS-CLI/osint-resource.sh'
-# SNS LIST
+
+alias resource='zsh RESOURCE'
+
+# Name of the function to be executed
 menu="
-   Chrome
-   Safari
-   Firefox
-   Tor
-   resource
+   Maps, Geolocation and Transport
+   Social Media
+   Domain/IP/Links
+   Image Search and Identification
+   Cryptocurrencies
+   Messengers
+   Code
+   Search engines
+   Tools for Google
+   IOT
+   Archives
+   Archives of documents
+   Datasets
+   Passwords, emails, phone numbers
+   People search
+   Sock Puppets
+   NOOSINT tools
+   Tools collections
+   Files
+   IMEI and serial numbers
+   Keywords, trends, news analytics
+   Apps and programs
+   Brands, companies, items
+   Amazon
+   Movies
+   TV/Radio
+   VirtualMachines/Linux distributions
+   My Projects
   ← exit
 "
 
-# SNS URL PUSH
-_sf_1 () { open -a "/Applications/Google Chrome.app" && clear }
-_sf_2 () { open -a "/Applications/Safari.app" | exit }
-_sf_3 () { open -a "/Applications/Firefox.app" | exit }
-_sf_4 () { open -a "/Applications/Tor Browser.app" | exit }
-_sf_5 () { resource }
-_sf_exit () { exit }
+# List of functions to assign to the list
+_sf_0 () { exit }
+_sf_1 () { exit }
+_sf_2 () { exit }
+_sf_3 () { clear && exit }
 
+# Search for a selected number of lines
 func (){
-  _sf_1 () {
-    _s_current_n=0
-    _s_break=1
-  }
-  _sf_2 () {
-    _s_current_n=1
-    _s_break=1
-  }
-  _sf_3 () {
-    _s_current_n=2
-    _s_break=1
-  }
-  _sf_4 () {
-    _s_current_n=3
-    _s_break=1
-  }
-  _sf_5 () {
-    _s_current_n=4
-    _s_break=1
-  }
-  _sf_exit () {
-    _s_current_n=5
-    _s_break=1
-  }
+  for i in `seq 0 99`
+    do
+    _sf_ + $i () {
+      _s_current_n=$i
+      _s_break=1
+    }
+    done
 }
 
+# Run the selected function.
 _sf_select () {
-  if   [ $_s_current_n -eq 0 ];then
-    _sf_1
-  elif [ $_s_current_n -eq 1 ];then
-    _sf_2
-  elif [ $_s_current_n -eq 2 ];then
-    _sf_3
-  elif [ $_s_current_n -eq 3 ];then
-    _sf_4
-  elif [ $_s_current_n -eq 4 ];then
-    _sf_5
-  elif [ $_s_current_n -eq 5 ];then
-    _sf_exit
-  fi
+  # The number of functions
+  for i in `seq 0 99`
+    do
+      if   [ $_s_current_n -eq $i ];then
+        _sf_$i
+      fi
+    done
 }
 
 echo "$menu" | _sf_main "$@"
-echo "hello"
